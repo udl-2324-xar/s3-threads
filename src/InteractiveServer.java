a438a17ebf09d46b2033daec42ff9f5e17b5f87a
Albert Rovira <diphone2020@gmail.com>
2024-02-20 08:20:11 +0100

    ending s2

--- src/InteractiveServer.java
+++ src/InteractiveServer.java
@@ -1,14 +1,41 @@
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class InteractiveServer {
     private static final int port = 1234;
 
     public static void main(String[] args){
-        try {
-            ServerSocket ss = new ServerSocket(port);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try
+        {
+            ServerSocket ss = new ServerSocket (port);
+
+            for (;;)
+            {
+                Socket s = ss.accept();
+                System.err.println ("Connexió acceptada.");
+                DataInputStream dis = new DataInputStream  (s.getInputStream());
+                DataOutputStream dos = new DataOutputStream (s.getOutputStream());
+                String str = "";
+
+                while (!str.equals ("FI"))
+                {
+                    str = dis.readUTF();
+                    System.out.println ("He rebut el missatge \"" + str + "\"");
+                    str = str.toUpperCase();
+                    dos.writeUTF (str);
+                    dos.flush();
+                }
+                dis.close();
+                dos.close();
+                s.close();
+                System.out.println ("Connexió tancada.");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
